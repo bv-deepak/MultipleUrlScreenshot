93bf044159bac8c35d6c80997288ee825b510554
bv-name <deepak@blogvault.net>
2019-01-25 16:57:05 +0530

    ssid_updation

--- db/migrate/20190125112408_add_column_to_screenshots.rb
+++ db/migrate/20190125112408_add_column_to_screenshots.rb
@@ -1,5 +1,5 @@
 class AddColumnToScreenshots < ActiveRecord::Migration[5.2]
   def change
-    add_column :screenshots, :message, :string
+    add_column :screenshots, :ssid, :string
   end
 end
--- lib/jobs/screenshot_job.rb
+++ lib/jobs/screenshot_job.rb
@@ -3,7 +3,8 @@ class ScreenshotJob
 	def perform(blog, snap_id)
 			pages = blog.pages
 			pages.each do |page|
-				Screenshot.capture_screenshot(page.url, blog.id, page.id, snap_id)
+				ssid = SecureRandom.hex(8)
+				Screenshot.capture_screenshot(page.url, blog.id, page.id, snap_id, ssid)
 			end
 	rescue => e
 		put e
